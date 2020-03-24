c1a35c0f72332bfe9b292c7bc9872a611c6de6b1
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-03-24 18:05:26 +0800

    返费人数

--- src/views/dashboard/PanelGroup.vue
+++ src/views/dashboard/PanelGroup.vue
@@ -110,6 +110,38 @@
       </div>
     </el-col>
   </el-row>
+
+  
+  <el-row :gutter="40" class="panel-group">
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+        </div>
+        <div class="card-panel-description">
+          <div class="card-panel-text">
+            返费人数
+          </div>
+          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+        </div>
+      </div>
+    </el-col>
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('messages')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+        </div>
+        <div class="card-panel-description">
+          <div class="card-panel-text">
+            奖励人数
+          </div>
+          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
+        </div>
+      </div>
+    </el-col>
+    
+  </el-row>
+
 </div>
   
 </template>
