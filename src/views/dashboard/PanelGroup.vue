8f7c5b924dc82fb753d6c52121011d99b068aa95
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-03-25 10:03:12 +0800

    奖励人数

--- src/views/dashboard/PanelGroup.vue
+++ src/views/dashboard/PanelGroup.vue
@@ -62,9 +62,13 @@
         <div class="card-panel-icon-wrapper icon-people">
           <svg-icon icon-class="peoples" class-name="card-panel-icon" />
         </div>
-        <div class="card-panel-description">
+        <div class="card-panel-description card-panel-description-defined">
           <div class="card-panel-text">
             返费金额
+          </div>
+          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+          <div class="card-panel-text">
+            返费人数
           </div>
           <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
         </div>
@@ -75,9 +79,13 @@
         <div class="card-panel-icon-wrapper icon-people">
           <svg-icon icon-class="peoples" class-name="card-panel-icon" />
         </div>
-        <div class="card-panel-description">
+        <div class="card-panel-description card-panel-description-defined">
           <div class="card-panel-text">
             奖励金额
+          </div>
+          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
+          <div class="card-panel-text">
+            奖励人数
           </div>
           <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
         </div>
@@ -112,33 +120,7 @@
   </el-row>
 
   
-  <el-row :gutter="40" class="panel-group">
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
-        <div class="card-panel-icon-wrapper icon-people">
-          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            返费人数
-          </div>
-          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('messages')">
-        <div class="card-panel-icon-wrapper icon-people">
-          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            奖励人数
-          </div>
-          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
+
     
   </el-row>
 
@@ -230,7 +212,9 @@ export default {
       float: left;
       font-size: 48px;
     }
-
+    .card-panel-description-defined{
+      margin:5px!important;
+    }
     .card-panel-description {
       float: right;
       font-weight: bold;
